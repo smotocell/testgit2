beadbdc512f584ad4662c35884ab26fe248ceb70
Dan Ilinca <ilinca_danutts@yahoo.com>
2018-07-26 22:48:23 +0300

    Initial Commit

---
+++ testgit2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testgit2
+//
+//  Created by Dan Ilinca on 26/07/2018.
+//  Copyright © 2018 Dan Ilinca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ testgit2Tests/testgit2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  testgit2Tests.swift
+//  testgit2Tests
+//
+//  Created by Dan Ilinca on 26/07/2018.
+//  Copyright © 2018 Dan Ilinca. All rights reserved.
+//
+
+import XCTest
+@testable import testgit2
+
+class testgit2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
